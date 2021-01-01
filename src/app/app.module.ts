2d77afafc7ab157ce76a3793e01c48dcc44744ef
randon93 <32597818+randon93@users.noreply.github.com>
2021-01-01 16:49:21 -0500

    Iniciando curso de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { SidersComponent } from './shared/siders/siders.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoPageFoundComponent,
+    DashBoardComponent,
+    SidersComponent,
+    HeaderComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
