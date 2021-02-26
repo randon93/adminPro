59434bbcf55bab808004cafa719bfc5bbe4aca56
randon93 <jbgm93@gmail.com>
2021-02-25 23:04:10 -0500

    agreganDo moDulos clase 33

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { DashBoardComponent } from './pages/dash-board/dash-board.component';
-import { SidersComponent } from './shared/siders/siders.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficaUnoComponent } from './pages/grafica-uno/grafica-uno.component';
-import { PagesComponent } from './pages/pages.component';
-
 
 @NgModule({
   declarations: [
@@ -22,19 +15,9 @@ import { PagesComponent } from './pages/pages.component';
     LoginComponent,
     RegisterComponent,
     NoPageFoundComponent,
-    DashBoardComponent,
-    SidersComponent,
-    HeaderComponent,
-    BreadcrumbsComponent,
-    ProgressComponent,
-    GraficaUnoComponent,
-    PagesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, PagesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
