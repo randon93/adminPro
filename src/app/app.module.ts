305733586594a4d644dc9d6aa249b3c39a82407c
randon93 <jbgm93@gmail.com>
2021-02-25 23:40:24 -0500

    AGREGANDO MODULO  PAGES.ROUTING

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { AuthModule } from './auth/auth.module';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NoPageFoundComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  declarations: [AppComponent, NoPageFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
